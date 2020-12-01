9dcd20ef4b92bf3acf32f9575c9dbceec419440f
unknown <jdemarryme@qq.com>
2020-12-01 23:02:59 +0800

    add note1

--- text-cnn/data_processor.py
+++ text-cnn/data_processor.py
@@ -4,25 +4,31 @@ import re
 from torchtext.vocab import Vectors
 
 
-def tokenizer(text): # create a tokenizer function
-    regex = re.compile(r'[^\u4e00-\u9fa5aA-Za-z0-9]')   # ^\u4e00-\u9fa 匹配汉字
+# 句子分词
+def tokenizer(text):  # create a tokenizer function
+    regex = re.compile(r'[^\u4e00-\u9fa5aA-Za-z0-9]')  # ^\u4e00-\u9fa 匹配汉字
     text = regex.sub(' ', text)
     return [word for word in jieba.cut(text) if word.strip()]
 
 
-# 返回停用词
+# 获取停用词
 def get_stop_words():
-    file_object = open('data/stopwords.txt', 'rb')   # 直接读数据
+    file_object = open('data/stopwords.txt', 'rb')  # 直接读数据
     stop_words = []
-    for line in file_object.readlines():    # 按行读取
+    for line in file_object.readlines():  # 按行读取
         # line = line[:-1]    # line的元素从0至位置-1（倒数第一个），相当于处于最后一个字符
-        line = line.strip()   # 去掉首位换行符
+        line = line.strip()  # 去掉首位换行符
         stop_words.append(line)
     return stop_words
 
+
+# 加载数据
 def load_data(args):
-    print('加载数据中...')
-    stop_words = get_stop_words() # 加载停用词表
+
+    # 加载停用词表
+    stop_words = get_stop_words()
+
+    # 定义数据字段
     '''
     如果需要设置文本的长度，则设置fix_length,否则torchtext自动将文本长度处理为最大样本长度
     text = data.Field(sequential=True, tokenize=tokenizer, fix_length=args.max_len, stop_words=stop_words)
@@ -30,36 +36,48 @@ def load_data(args):
     text = data.Field(sequential=True, lower=True, tokenize=tokenizer, stop_words=stop_words)
     label = data.Field(sequential=False)
 
-    text.tokenize = tokenizer
+    # 读取数据并根据定义的字段加载数据
     train, val = data.TabularDataset.splits(
-            path='data/',
-            skip_header=True,
-            train='train.tsv',
-            validation='validation.tsv',
-            format='tsv',
-            fields=[('index', None), ('label', label), ('text', text)],
-        )
+        path='data/',
+        skip_header=True,
+        train='train.tsv',
+        validation='validation.tsv',
+        format='tsv',
+        fields=[('index', None), ('label', label), ('text', text)],
+    )
 
     # 加载静态词向量
     if args.static:
-        text.build_vocab(train, val, vectors=Vectors(name="data/eco_article.vector")) # 此处改为你自己的词向量
+        text.build_vocab(train, val, vectors=Vectors(name="data/eco_article.vector"))  # 此处改为你自己的词向量
         args.embedding_dim = text.vocab.vectors.size()[-1]
-        args.vectors = text.vocab.vectors   # 返回词汇表
+        args.vectors = text.vocab.vectors  # 返回词汇表
     # 构建动态词向量，训练自动调整
     else:
         text.build_vocab(train, val)
 
-    # 标签也要build？这里标签可以不用build
+    # 此处与后面训练模型时标签-1相对应，并无太大意义
+    # 标签build后：对于无标签<unk>为0，有标签0和1在build之后分别为1和2
     label.build_vocab(train, val)
 
-    # TODO: sort_key 有点不太懂
+    # 构建batch大小的数据集
+    '''
+    torchtext.data.Iterators
+        Source: https://pytorch.org/text/stable/data.html
+    
+        (1) 这里sort_key根据文本的长度进行排序
+            sort_key用于对示例进行排序的键，以便将具有相似长度的示例批量组合在一起并最小化填充
+        (2) lambda可以理解为箭头函数，输入x返回x的text属性值的长度
+        (3) 这里val验证集的batch_size则正好为验证集的长度，不用进行细分
+        (4) 这里device用于指定创建变量的设备（默认为cpu），如果使用gpu需要指定设备字符串
+    '''
     train_iter, val_iter = data.Iterator.splits(
-            (train, val),
-            sort_key=lambda x: len(x.text), # text是文件中读取出的列主键，
-            batch_sizes=(args.batch_size, len(val)), # 训练集设置batch_size,验证集整个集合用于测试
-            device=-1
+        (train, val),
+        sort_key=lambda x: len(x.text),  # text是文件中读取出的列主键
+        batch_sizes=(args.batch_size, len(val)),  # 训练集设置batch_size,验证集整个集合用于测试
+        device=-1
     )
-    args.vocab_size = len(text.vocab)
-    args.label_num = len(label.vocab)
-    return train_iter, val_iter
 
+    args.vocab_size = len(text.vocab)   # 文本词汇表的数量（单词转为不同数字的数量）
+    args.label_num = len(label.vocab)   # 标签词汇表的数量
+
+    return train_iter, val_iter
--- text-cnn/model.py
+++ text-cnn/model.py
@@ -5,59 +5,82 @@ import torch.nn.functional as F
 
 
 class TextCNN(nn.Module):
-    # 多通道textcnn
+    # 初始化模型参数
     def __init__(self, args):
         super(TextCNN, self).__init__()
+
+        # 模型全部的参数
         self.args = args
 
-        label_num = args.label_num  # 标签的个数
-        filter_num = args.filter_num    # 卷积核的个数
-        filter_sizes = [int(fsz) for fsz in args.filter_sizes.split(',')]   # filter_sizes数组：[3,4,5]
+        # 标签词汇表的长度（由数据集确定）
+        label_num = args.label_num
 
-        vocab_size = args.vocab_size    # ？？？
+        # 卷积核的个数（由使用者设定）
+        filter_num = args.filter_num
+
+        # 卷积核大小数组（论文标准为[3,4,5]）
+        filter_sizes = [int(fsz) for fsz in args.filter_sizes.split(',')]
+
+        # 文本词汇表的长度（由数据集确定）
+        vocab_size = args.vocab_size
+
+        # 词向量的维度（由词向量表确定，根据具体使用的词向量的维度确定）
         embedding_dim = args.embedding_dim
 
+        # 词向量矩阵
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        if args.static: # 如果使用预训练，词向量则提前加载，当不需要微调时设置freeze为True
+
+        # 静态词向量（如果使用预训练，词向量则提前加载，当不需要微调时设置freeze为True）
+        if args.static:
             self.embedding = self.embedding.from_pretrained(args.vectors, freeze=not args.fine_tune)
 
-        self.convs = nn.ModuleList(
+        # 神经网络模型
+        self.conv = nn.ModuleList(
             [nn.Conv2d(1, filter_num, (fsz, embedding_dim)) for fsz in filter_sizes])
+
+        # dropout（随机丢弃，论文标准为0.5）
         self.dropout = nn.Dropout(args.dropout)
-        self.linear = nn.Linear(len(filter_sizes)*filter_num, label_num)    # 输出维度：len(filter_sizes)*filter_num
 
+        # 确定最后的输出向量维度：len(filter_sizes) * filter_num
+        self.linear = nn.Linear(len(filter_sizes) * filter_num, label_num)
+
+    # 前馈网络
     def forward(self, x):
-        # 输入x的维度为(batch_size, max_len), max_len可以通过torchtext设置或自动获取为训练样本的最大=长度
-        x = self.embedding(x) # 经过embedding,x的维度为(batch_size, max_len, embedding_dim)
 
-        # 经过view函数x的维度变为(batch_size, input_chanel=1, w=max_len, h=embedding_dim)
+        # 输入x的维度为 (batch_size, max_len)
+        #   max_len可以通过torchtext设置或自动获取为训练样本的最大长度
+
+        # 经过embedding,x的维度为 (batch_size, max_len, embedding_dim)
+        x = self.embedding(x)
+
+        # 经过view函数x的维度变为 (batch_size, input_channel=1, w=max_len, h=embedding_dim)
         x = x.view(x.size(0), 1, x.size(1), self.args.embedding_dim)
 
-        # 经过卷积运算,x中每个运算结果维度为(batch_size, out_chanel, w, h=1)
-        x = [F.relu(conv(x)) for conv in self.convs]
+        # 经过卷积运算,x中每个运算结果维度为(batch_size, out_channel, w=max_len, h=1)
+        x = [F.relu(conv(x)) for conv in self.conv]
 
-        # 经过最大池化层,维度变为(batch_size, out_chanel, w=1, h=1)
+        # 经过最大池化层,维度变为(batch_size, out_channel, w=1, h=1)
         x = [F.max_pool2d(input=x_item, kernel_size=(x_item.size(2), x_item.size(3))) for x_item in x]
 
-        # 将不同卷积核运算结果维度（batch，out_chanel,w,h=1）展平为（batch, outchanel*w*h）
+        # 将不同卷积核运算结果维度 (batch, out_channel, w=1, h=1) 展平为 (batch, out_channel * w * h)
         x = [x_item.view(x_item.size(0), -1) for x_item in x]
 
-        # 将不同卷积核提取的特征组合起来,维度变为(batch, sum:outchanel*w*h)
+        # 将不同卷积核提取的特征组合起来,维度变为 (batch, sum:out_channel * w * h)
         x = torch.cat(x, 1)
 
         # dropout层
         x = self.dropout(x)
 
         # 全连接层
-        logits = self.linear(x)
-        return logits
+        logistic = self.linear(x)
+        return logistic
 
         # forward的另外一种写法
         # x = self.embedding(x)
         # x = x.unsqueeze(1)
-        # x = [F.relu(conv(x)).squeeze(3) for conv in self.convs]
+        # x = [F.relu(conv(x)).squeeze(3) for conv in self.conv]
         # x = [F.max_pool1d(item, item.size(2)).squeeze(2) for item in x]
         # x = torch.cat(x, 1)
         # x = self.dropout(x)
-        # logits = self.fc(x)
-        # return logits
+        # logistic = self.fc(x)
+        # return logistic
